9c5560b912ceef54b2fb568d778cafe1801f3e15
Ross Brook <rossjohnbrook@googlemail.com>
2021-06-16 01:21:12 +0100

    added further comments

--- Functions1.py
+++ Functions1.py
@@ -12,5 +12,5 @@ def lesser_of_two_evens(a,b):     #A function that will retun the lesser of 2 nu
         else:
             result = b
     print(result)
-lesser_of_two_evens(5,7)
-lesser_of_two_evens(2,4)
+lesser_of_two_evens(5,7)  #this will return 7 this will return 7 as its lower and they are both 
+lesser_of_two_evens(2,4)  #this will return 2 as its lower and they are both even
