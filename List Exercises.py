6b012a00ce8fac69f7ddd65cbde077da5c9f8d32
Ross Brook <rossjohnbrook@googlemail.com>
2021-06-18 05:49:42 +0100

    list exercises

--- List Exercises.py
+++ List Exercises.py
@@ -19,3 +19,21 @@ for x, y in zip(list4, list5[::-1]):
 list6=['Mike', '', 'Emma', '', 'Kelly', '', 'Brad']  
 reslist = list(filter(None, list6))
 print(reslist)
+
+#appending a set number to a set place in a list (7000 after 6000)
+list7 = [10, 20, [300, 400, [5000, 6000],500], 30, 40]   
+list7[2][2].append(7000)
+print(list7)
+
+
+#sublist to be added to a list (adding h,i,j after the g)
+list8=['a', 'b', ['c', ['d', 'e', ['f', 'g'], 'k'], 'l'], 'm', 'n']     
+sublist = ['h', 'i', 'j']
+list8[2][1][2].extend(sublist)
+print(list8)
+
+#replacing a certain number with another number when it appears in a list (20 for 200)
+list9 = [5, 10, 15, 20, 25, 50 ,20]   
+index = list9.index(20)
+list9[index] = 200
+print(list9)
