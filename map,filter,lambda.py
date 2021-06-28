d1cffe816693a08758c6f4c18acb1ecda661fd1b
Ross Brook <rossjohnbrook@googlemail.com>
2021-06-28 22:39:40 +0100

    more stuff

--- map,filter,lambda.py
+++ map,filter,lambda.py
@@ -33,13 +33,46 @@ names_of_people =['Ross', 'Chelsea', 'Hallie', 'Bonnie']
 list(map(lambda letter: letter[0],names_of_people))
 #this would return ['R','C','H','B']
 names_of_people =['Ross', 'Chelsea', 'Hallie', 'Bonnie']
-list(map(lambda letter: letter[::-1],names_of_people))
+list(map(lambda letter: letter[::-1],names_of_people)) 
 #this would return ['ssoR','aeslehC','eillaH','einnoB']
+
+my_numbers = [1,2,3,4,5]
+double_num = list(map(lambda num: num *2,my_numbers)) #will return a list of nums in my_numbers
+print(double_num)                                     #will print the list
 
 #-------------------
 #filter allows you to filter results
 def check_even(num):
     return num%2 == 0
 mynums = [1,2,3,4,5,6,7,8,9,10]
-for n in filter(check_even,mynums):     #the filter function will only return even numbers while if we used the map() it would return either true or false for all numbers in mynums
+for n in filter(check_even,mynums):     #the filter function will only retur n even numbers while if we used the map() it would return either true or false for all numbers in mynums
     print(n)
+#count upper lower, .isupper() .islower()
+def up_low(s):
+
+    lowercase = 0
+    uppercase = 0
+    for char in s:
+        if char.isupper():
+            uppercase += 1
+        elif char.islower():
+            lowercase += 1
+        else:
+            pass
+    print(f'Original String : {s}')
+    print(f'No. of Upper case characters : {uppercase}')
+    print(f'No. of Lower case characters : {lowercase}')
+    
+#^^^^^^^^^but using a dictionary 
+def up_low(s):
+    d = {'uppercase': 0,'lowercase': 0}
+    for char in s:
+        if char.isupper():
+            d['uppercase']+= 1
+        elif char.islower():
+            d['lowercase'] += 1
+        else:
+            pass
+    print(f'Original String : {s}')
+    print(f'No. of Upper case characters : {d["uppercase"]}')
+    print(f'No. of Lower case characters : {d["lowercase"]}')
